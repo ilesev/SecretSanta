a65b997910258e70a465c7ecf9d1cf9309222a83
ilesev <ilesev@shutterfly.com>
2018-01-29 18:17:49 +0200

    Initial commit

---
+++ SecretSanta/Controllers/RegistrationController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace SecretSanta.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public String myConfig {get;set;}
+        public IConfiguration Configuration
+        {
+            get;
+            set;
+        }
+
+        public RegistrationController(IConfiguration Configuration)
+        {
+            this.Configuration = Configuration;
+            myConfig = Configuration["DbConnectionString"];
+        }
+
+        [Route("api")]
+        public String Register()
+        {
+            using(var conn = new MySqlConnection(myConfig)) 
+            {
+                conn.OpenAsync();
+                MySqlCommand command = new MySqlCommand("SELECT * FROM accounts", conn);
+                using(MySqlDataReader reader = command.ExecuteReader()) {
+                    while(reader.Read()) {
+                        Console.WriteLine(reader.GetString("username"));
+                    }
+                }
+            }
+
+            return myConfig;
+        }
+    }
+}
